8f58903c426529b53ae290ea43df5715a0397a47
16tja <16tja@queensu.ca>
2021-03-19 11:28:41 -0400

    Subsetting the data

---
+++ Subsetting the data.R
@@ -0,0 +1,10 @@
+dat <- read.csv('./Dataset/sequences.csv')
+library(dplyr)
+library(stringr)
+dat <- dat %>% filter(Nuc_Completeness == 'complete') %>% filter(Geo_Location != '')
+
+dat_USA <- dat %>% filter(str_detect(Geo_Location, 'USA'))
+dat_CHINA <- dat %>% filter(str_detect(Geo_Location, 'China'))
+dat_AUSTRALIA <- dat %>% filter(str_detect(Geo_Location, 'Australia'))
+dat_INDIA <- dat %>% filter(str_detect(Geo_Location, 'India'))
+dat_JAPAN<- dat %>% filter(str_detect(Geo_Location, 'Japan'))
