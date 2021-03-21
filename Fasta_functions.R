c9eaa8dc2cd1f89202e1f3f97208e17f918059e2
Christian Garnons-Williams <christian.garnonswilliams@gmail.com>
2021-03-21 00:33:01 -0400

    Changing the regex in separate_fasta() to detect both new line (/n) and carriage return (/r) characters.

--- Fasta_functions.R
+++ Fasta_functions.R
@@ -2,20 +2,19 @@
 Title:   Fasta_functions
 Made by: Christian Garnons-Williams
 Made on: 2021-03-18
+Version: 2 (2021-03-21)
 "
-
 
 # Takes String FASTA file as input
 # Returns dataframe w/ w/ 1 col for header, & 1 for raw sequence
 # with each row representing a single FASTA entry from the FASTA file
 separate_fasta <- function(fasta){
    fasta <- unlist(strsplit(fasta, split = "\n\n"))
-   header <- gsub("(^>.*genome)\\n.*", "\\1", fasta)
-   seq <- gsub("^>.*genome\\n(.*)", "\\1", fasta)
-   seq <- gsub("\\n", "", seq)
+   header <- gsub("(^>.*genome)(\\n|\\r).*", "\\1", fasta)
+   seq <- gsub("^>.*genome(\\n|\\r)(.*)", "\\2", fasta)
+   seq <- gsub("\\n|\\r", "", seq)
    return(data.frame(Name = header, Sequence = seq))
 }
-
 
 
 # Takes df created by *separate_fasta*, and a start seq, and end seq (as String)
