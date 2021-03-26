a7975d592d597caff23573f418e668469126abe6
Christian Garnons-Williams <christian.garnonswilliams@gmail.com>
2021-03-26 15:11:56 -0400

    Debugging, removing redundancy, and trimming number of columns for ease of working. Also added rm() at end of file to remove excess files once the file has run

--- Subsetting the data.R
+++ Subsetting the data.R
@@ -1,21 +1,42 @@
-dat <- read.csv('./Dataset/sequences.csv')
 library(dplyr)
 library(stringr)
 
-dat <- read.csv('./Dataset/sequences.csv')
+# Loading Sequence data & filtering by completeness, trimming un-used cols
+dat <- read.csv('./Dataset/sequences.csv') %>%
+  filter(Nuc_Completeness == "complete") %>%
+  transmute(Accession, Length, Geo_Location)
 
-# Filter dataset by completeness
+# Setting number of samples per country:
+N <- 100
 
-dat_USA <- dat %>% filter(str_detect(Geo_Location, 'USA')) %>% mutate(Geo_Location = "USA")
-                %>% slice_sample(n = 100)
-dat_CHINA <- dat %>% filter(str_detect(Geo_Location, 'China')) %>% mutate(Geo_Location = "CHN")
-                %>% slice_sample(n = 100)
-dat_AUSTRALIA <- dat %>% filter(str_detect(Geo_Location, 'Australia')) %>% mutate(Geo_Location = "AUS")
-                %>% slice_sample(n = 100)
-dat_INDIA <- dat %>% filter(str_detect(Geo_Location, 'India')) %>% mutate(Geo_Location = "IND")
-                %>% slice_sample(n = 100)
-dat_JAPAN<- dat %>% filter(str_detect(Geo_Location, 'Japan')) %>% mutate(Geo_Location = "JPN")
-                %>% slice_sample(n = 100)
+# Filter dataset by country, and sampling N random samples from each
+dat_USA <- dat %>%
+  filter(str_detect(Geo_Location, 'USA')) %>%
+  mutate(Geo_Location = "USA") %>%
+  slice_sample(n = N)
+
+dat_CHINA <- dat %>%
+  filter(str_detect(Geo_Location, 'China')) %>%
+  mutate(Geo_Location = "CHN") %>%
+  slice_sample(n = N)
+
+dat_AUSTRALIA <- dat %>%
+  filter(str_detect(Geo_Location, 'Australia')) %>%
+  mutate(Geo_Location = "AUS") %>%
+  slice_sample(n = N)
+
+dat_INDIA <- dat %>%
+  filter(str_detect(Geo_Location, 'India')) %>%
+  mutate(Geo_Location = "IND") %>%
+  slice_sample(n = N)
+
+dat_JAPAN <- dat %>%
+  filter(str_detect(Geo_Location, 'Japan')) %>%
+  mutate(Geo_Location = "JPN") %>%
+  slice_sample(n = N)
 
 # Dataset with the selected countries
-countries <- data.frame(dat_JAPAN, dat_INDIA, dat_AUSTRALIA, dat_CHINA, dat_USA)
+countries <- rbind(dat_JAPAN, dat_INDIA, dat_AUSTRALIA, dat_CHINA, dat_USA)
+
+# Removing the temp data files
+rm(dat_AUSTRALIA, dat_CHINA, dat_USA, dat_INDIA, dat_JAPAN)
