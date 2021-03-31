34d8c14a87014234447a5248de66a2f4002a99d9
Christian Garnons-Williams <christian.garnonswilliams@gmail.com>
2021-03-31 00:25:45 -0400

    Mutation comparison NOW WORKS!! Changes are massive: 1) mutations_ref now uses BP position relative to start of protein in the Change column 2) CHANGES TO df_isolate!! Mutation information is now stored as a series of LISTS within df_isolate$Mutation. You can access individual elements with the following notation: to access the 1st mutation of row 15: df_isolate$Mutations[[15]][1] 2nd mutation of row 15: df_isolate$Mutations[[15]][2] etc.

--- Mutation_functions.R
+++ Mutation_functions.R
@@ -1,23 +1,10 @@
 "
 Title:    Mutation_functions
 Made by:  Christian Garnons-Williams
-Made on:  2021-03-25
-Version:  1
+Made on:  2021-03-30
+Version:  4
 Requires: dplyr must be loaded for all functions to run
 "
-
-########### Work with the Extracted mutations ###########
-
-# Building the 'not in' operator for important future uses
-'%notin%' <- Negate('%in%') # Took me a half an hour to get this working smh
-
-# Function to isolate mutations from output of Alignment.R processes
-# Takes processed dataframe with mutations identified
-# Returns dataframe containing only
-get_muts <- function(mutant_df){
-  not_mut = c("haracter0", "Reference")
-  return(mutant_df %>% filter(Mutations %notin% not_mut))
-}
 
 ########### Work with the reference mutations ###########
 
@@ -27,18 +14,63 @@ mutations <- read.csv("./Dataset/mutations.csv")
 # Function for extracting usable mutations from the reference mutation dataset
 extract_mut <- function(raw_muts, pos){
   muts <- strsplit(raw_muts, split = " > ")[[1]]
-  origin <- strsplit(muts, split = "")[[1]]
-  new <- strsplit(muts, split = "")[[2]]
+  origin <- strsplit(muts, split = "")[[2]] # flipped here to be compatible with our dataset
+  new <- strsplit(muts, split = "")[[1]]
   for (i in c(1:3)){
     if (!grepl(origin[i], new[i])){
-      return(paste((i-1 + pos), ":" , tolower(origin[i]), "->", tolower(new[i]), sep=""))
+      return(paste((pos - 21562), ":" , tolower(origin[i]), "->", tolower(new[i]), sep=""))
     }
   }
 }
+
 
 # Dyplr code to convert the mutations into something usable
 mutations_ref <- mutations %>%
   group_by(row.names(mutations)) %>%
   mutate(Change = extract_mut(Change,Location))
 
-rm(mutations)
+# Remove row.names columns
+mutations_ref <- mutations_ref[-8]
+
+# Removing the temp variable
+rm(mutations, extract_mut)
+
+
+
+########### Work with the Extracted mutations ###########
+
+# Function searching through mutations_ref using df_isolate mutations.
+# Takes a single row's cleaned mutations & the reference file as argument.
+# Returns vector of mutation names from the reference data if found;
+# if match not found, returns unchanged mutation notation.
+# Called by "clean_mut()"
+get_name <- function(muts, ref_muts){
+  for (i in c(1:length(muts))){
+    result = grep(paste("^", muts[i], sep=""), ref_muts$Change)
+    if (length(result) > 0){ # If match found
+      muts[i] = ref_muts$Designation[result]
+    }
+  }
+  return (muts)
+}
+
+# Function to extract mutation info from a single row of df_isolate mutations.
+# Takes a single row's raw mutations & the reference file as argument.
+# Returns LIST of mutation names from the reference data if found;
+# if match not found, cleaned mutation notation is returned instead.
+# Calls: get_name(), to perform comparison to reference data
+clean_mut <- function(mut, ref_muts){
+  not_mut = c("haracter0", "Reference")
+  if (mut %in% not_mut){ # If no mutation:
+    newmut = list("Reference")
+  } else{ # If mutation:
+    newmut = gsub("[\"\\]", "", strsplit(mut, ", ")[[1]])
+    newmut = list(get_name(newmut, ref_muts)) # Calls get_name()^
+  }
+  return (newmut) # Returns as list, so it can be stored in dataframe
+}
+
+# Dplyr code to rapidly get through the
+df_isolate <- df_isolate %>%
+  group_by(row.names(df_isolate)) %>%
+  mutate(Mutations = I(clean_mut(Mutations, mutations_ref))) # calls clean_mut()
